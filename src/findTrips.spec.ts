4fe82db166b8366e634c6b1735edbb889fa3f29e
Mike Hodnick <mike@kindohm.com>
2023-03-18 12:00:57 -0500

    recursive hell

--- src/findTrips.spec.ts
+++ src/findTrips.spec.ts
@@ -1,49 +1,92 @@
 import { findTrips } from "./findTrips";
 import { Point } from "./points";
+import { Segment } from "./getAllSegments";
+
+const points: Point[] = [
+  { name: "asdf", mile: 1, type: "campsite", id: "point0" },
+];
+
+const mockSegment: Segment = {
+  points: [],
+  startsAtTrailhead: false,
+  endsAtTrailhead: false,
+  name: "",
+  reverseName: "",
+  distance: 1,
+  startPointId: "",
+  endPointId: "",
+};
 
 describe("findTrips", () => {
-  it("should return empty array when max distance is zero", () => {
-    const points: Point[] = [
-      { name: "asdf", mile: 1, type: "campsite", id: "point0" },
-    ];
-    const maxDailyDistance = 0;
-    const trips = findTrips({ points, maxDailyDistance, numberOfDays: 1 });
+  it("should return empty array when there are no segments", () => {
+    const numberOfDays = 10;
+    const segments: Segment[] = [];
+
+    const trips = findTrips({
+      segments,
+      numberOfDays,
+    });
+
     expect(trips.length).toEqual(0);
   });
 
-  it("should start and end at trailhead", () => {
-    const points: Point[] = [
-      { name: "1", mile: 1, type: "campsite", id: "point0", nextId: 1 },
-      {
-        name: "2",
-        mile: 2,
-        type: "trailhead",
-        id: "point1",
-        nextId: 2,
-        previousId: 0,
-        distanceToParking: 1,
-      },
-      {
-        name: "3",
-        mile: 3,
-        type: "campsite",
-        id: "point2",
-        nextId: 3,
-        previousId: 1,
-      },
-      {
-        name: "4",
-        mile: 4,
-        type: "trailhead",
-        id: "point3",
-        nextId: 4,
-        previousId: 2,
-        distanceToParking: 1,
-      },
-      { name: "5", mile: 5, type: "campsite", id: "point4", previousId: 3 },
+  it("should return empty when there is one segment that does not start or end at trailhead", () => {
+    const segments: Segment[] = [
+      { ...mockSegment, startsAtTrailhead: false, endsAtTrailhead: false },
     ];
 
-    const trips = findTrips({ points, maxDailyDistance: 100, numberOfDays: 1 });
+    const numberOfDays = 10;
+
+    const trips = findTrips({
+      segments,
+      numberOfDays,
+    });
+
+    expect(trips.length).toEqual(0);
+  });
+
+  it("should return empty when there is one segment and it does not start at trailhead", () => {
+    const segments: Segment[] = [
+      { ...mockSegment, startsAtTrailhead: false, endsAtTrailhead: true },
+    ];
+
+    const numberOfDays = 10;
+
+    const trips = findTrips({
+      segments,
+      numberOfDays,
+    });
+
+    expect(trips.length).toEqual(0);
+  });
+
+  it("should return empty when there is one segment and it does not end at trailhead", () => {
+    const segments: Segment[] = [
+      { ...mockSegment, startsAtTrailhead: true, endsAtTrailhead: false },
+    ];
+
+    const numberOfDays = 10;
+
+    const trips = findTrips({
+      segments,
+      numberOfDays,
+    });
+
+    expect(trips.length).toEqual(0);
+  });
+
+  it("should return one trip when there is one segment that starts and ends at trailheads", () => {
+    const segments: Segment[] = [
+      { ...mockSegment, startsAtTrailhead: true, endsAtTrailhead: true },
+    ];
+
+    const numberOfDays = 10;
+
+    const trips = findTrips({
+      segments,
+      numberOfDays,
+    });
+
     expect(trips.length).toEqual(1);
   });
 });
--- src/findTrips.ts
+++ src/findTrips.ts
@@ -1,26 +1,62 @@
 import { Segment } from "./getAllSegments";
-import { Point } from "./points";
 
 type Trip = {
   segments: Segment[];
 };
 
+type SegmentTree = Segment & {
+  possibleNextSegments?: SegmentTree[];
+};
+
+const addChildSegments = (
+  segment: Segment,
+  allSegments: Segment[],
+  numberOfDays: number,
+  currentDepth: number
+): SegmentTree => {
+  if (currentDepth > numberOfDays) {
+    return { ...segment, possibleNextSegments: [] };
+  }
+
+  const following = allSegments.filter(
+    (s) => s.startPointId === segment.endPointId
+  );
+  const followingWithChildren = following.map((f) =>
+    addChildSegments(f, allSegments, numberOfDays, currentDepth + 1)
+  );
+  return { ...segment, possibleNextSegments: followingWithChildren };
+};
+
+const getTripsFromTrees = (segments: SegmentTree[]): Trip[] => {};
+
 export const findTrips = ({
-  points,
+  segments,
   numberOfDays,
-  maxDailyDistance,
 }: {
-  points: Point[];
+  segments: Segment[];
   numberOfDays: number;
-  maxDailyDistance: number;
 }): Trip[] => {
-  if (
-    !points ||
-    points.length === 0 ||
-    maxDailyDistance <= 0 ||
-    numberOfDays <= 0
-  ) {
+  if (!segments || segments.length === 0 || numberOfDays <= 0) {
     return [];
   }
-  return [];
+
+  const additionalDays: number[] = new Array(numberOfDays - 1)
+    .fill(null)
+    .map((x, i) => i);
+
+  const segmentsStartingAtTrailheads = segments.filter(
+    (s) => s.startsAtTrailhead
+  );
+
+  const segmentsWithChildren = segmentsStartingAtTrailheads.map((s) =>
+    addChildSegments(s, segments, numberOfDays, 1)
+  );
+
+  return getTripsFromTrees(segmentsWithChildren);
+
+  // const tripsThatEndAtTrailhead = possibleTrips.filter((trip) => {
+  //   return trip.segments[trip.segments.length - 1].endsAtTrailhead;
+  // });
+
+  // return tripsThatEndAtTrailhead;
 };
